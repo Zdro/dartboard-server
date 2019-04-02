ccd37a109bf1f95c0167e0547a6b3e83a6f87744
Rémi ZDROJEWSKI <zdrojewski.remi@gmail.com>
2019-04-02 18:52:26 +0200

    UI changes

---
+++ model/gameVariants/Game501.js
@@ -0,0 +1,54 @@
+let Game = require('../game.js');
+let Dart = require('../dart.js');
+
+module.exports = class Game501 extends Game{
+    constructor(playerNames) {
+        super(playerNames);
+        this.objective = {
+            score : 501
+        };
+    }
+
+    isPlayerWinner(player) {
+        return player.getScore().score == this.objective.score
+    }
+
+    addDart(name, score, multiplier) {
+        if (this.waitingForNextRound) return false;
+        if (this.winnerIdx != undefined) return false;
+
+        let dart = new Dart(name, score, multiplier);
+
+        if (this.currentPlayer().getScore().score + dart.getScore() > this.objective.score){
+            this.currentPlayer().addDart(dart);
+            this.stopRound();
+            this.updateView();
+            return false;
+        }
+
+        return super.addDart(name, score, multiplier);
+    }
+    
+    playerScore(player) {
+        let score = 0;
+        let that = this;
+        player.getRounds().forEach(round => {
+            let roundScore = 0;
+            round.getDarts().forEach(dart => {
+                roundScore += dart.getScore();
+            });
+
+            if (score + roundScore <= that.objective.score){
+                score += roundScore;
+            }
+        });
+        return {score : score};
+    }
+    roundIsComplete() {
+        return this.currentPlayer().getCurrentRound().isComplete();
+    }
+
+    static getName(){
+        return '501'
+    };
+}
--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -1,21 +1,14 @@
-var game = null;
+let game = null;
 
 $(function () {
-  var socket = io();
+  let socket = io();
 
   function generatePlayersTable(game) {
-    var table = '';
-    if (game.winnerIdx != undefined) {
-      table += `<h3>Winner ! ${game.players[game.winnerIdx].name}</h3>`
-    } else if (game.waitingForNextRound)
-      table += '<h3>Waiting for next turn...</h3>'
-    else {
-      table += `<h3>${game.players[game.currentPlayerIdx].name} is playing...</h3>`
-    }
+    let table = '<h3>&nbsp;</h3>';
     table += `<table style="table-layout: fixed">
                 <thead>
                   <tr>
-                    <th style="width:75px"></th>`;
+                    <th style="width:12em"></th>`;
     game.players.forEach((player, i) => {
       table += `    <th class="center-align ${i == game.currentPlayerIdx ? 'light-blue lighten-4' : ''}"><h2 style="margin:0px">${player.name}</h2></th>`;
     });
@@ -23,7 +16,7 @@ $(function () {
                 </thead>
               <tbody>
                 <tr>
-                  <td class="center-align" style="font-weight:bold">Score</td>`;
+                  <td class="center-align" style="font-weight:bold"><h2 style="margin:0px">Score</h2></td>`;
     game.players.forEach((player, i) => {
       table += `  <td class="center-align lighten-4 ${i == game.currentPlayerIdx ? 'light-blue' : ''}"><h2 style="margin:0px">${player.score.score}</h2></td>`;
     });
@@ -54,29 +47,75 @@ $(function () {
     return table;
   }
 
+  function displayCurrentRound(game){
+    let currentRound;
+    let currentPlayer = game.currentPlayerIdx;
+    let dartLeft;
+
+    if (game.waitingForNextRound){
+      currentPlayer = currentPlayer - 1 < 0 ? game.players.length - 1 : currentPlayer - 1;
+      currentRound = game.players[currentPlayer].rounds[game.players[currentPlayer].rounds.length - 2]
+    }
+    else{
+      currentRound = game.players[currentPlayer].rounds[game.players[currentPlayer].rounds.length - 1]
+    }
+    dartLeft = 3 - currentRound.darts.length;
+
+    let table = '';
+    if (game.winnerIdx != undefined) {
+      table += `<h3>Winner ! ${game.players[game.winnerIdx].name}</h3>`
+    } else if (game.waitingForNextRound)
+      table += '<h3>Waiting for next turn...</h3>'
+    else {
+      table += `<h3>${game.players[game.currentPlayerIdx].name} is playing... ${dartLeft} darts left</h3>`
+    }
+
+    table += `<table style="table-layout: fixed">
+                <thead></thead>
+                <tbody>
+            `;
+    for (let i = 0; i < currentRound.darts.length; i++){
+      table += `  <tr>
+                    <td class="center-align" style="font-weight:bold">
+                      <h2 style="margin:0px">
+                        ${i + 1}
+                      </h2>
+                    </td>
+                    <td class="center-align" style="font-weight:bold">
+                      <h2 style="margin:0px">
+                        ${currentRound.darts[i].name.substr(0,1).toUpperCase() + ' ' + currentRound.darts[i].name.substr(1) }
+                      </h2>
+                    </td>
+                  </tr>
+
+      `
+    }
+
+    table += `</tbody>
+            </table`;
+
+    $('#currentRound').html(table);
+    console.log(currentRound);
+  }
+
   $("#dartboard path, #d25, #start_round, #stop_round").click(function () {
     socket.emit('hit', this.id);
   });
 
   socket.on('game', function (data) {
-    var obj = JSON.parse(data);
+    let obj = JSON.parse(data);
     game = obj;
     $('#scoreTable').html(generatePlayersTable(game));
-    
-    var rounds = game.players[game.currentPlayerIdx].rounds
-    var darts = {}
-    var lastDart = {}
-    if (rounds.length > 0){
-      darts = rounds[rounds.length - 1].darts
-      if (darts.length > 0)
-        lastDart = darts[darts.length - 1]
-  
-    }
-    console.log(
-
-      JSON.stringify(lastDart, null,2)
-      )
-     		
+    displayCurrentRound(game);    		
     $('.brand-logo').text(game.name);
   })
+
+  $(document).keypress(function(e) {
+    if (e.key == ' '){
+      $('#start_round').click();
+    }
+    else if (e.key == 'Enter'){
+      $('#stop_round').click();
+    }
+  });
 });
